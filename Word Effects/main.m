c56df7796f327ab7e933f0b60e89a81aaa76b02d
Alejandro Zielinsky <azielinsky95@gmail.com>
2018-04-09 13:58:18 -0400

    Initial Commit

---
+++ Word Effects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Word Effects
+//
+//  Created by Alejandro Zielinsky on 2018-04-09.
+//  Copyright © 2018 Alejandro Zielinsky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
